46def2d5c40bcd117cf154a4dc0dd8f03fc9e0cd
Tobi Okanlawon <tobiinlondon34@gmail.com>
2023-09-21 17:57:18 +0100

    Add basic markup for test website

---
+++ script.js
@@ -0,0 +1,11 @@
+// const statusContainer = document.getElementById("status");
+// const form = document.getElementById("main-form");
+
+// const handleForm = (e) => {
+//     return (
+// 	e.preventDefault();
+// 	alert("form submitted", e)
+//     )
+// }
+
+// form.addEventListener('submit', handleForm);
